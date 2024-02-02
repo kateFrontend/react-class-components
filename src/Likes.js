abee5914233e216e3079a32fc05b28ad88965732
Kate Danilova <ecdanilova@gmail.com>
2024-02-01 20:09:24 -0500

    create class component

---
+++ src/Likes.js
@@ -0,0 +1,17 @@
+/* React Functional Component 
+
+function Likes() {
+    return(<div></div>)
+} */
+
+// React Class Component
+
+export class Likes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        likes: 0
+    }
+  }
+}
